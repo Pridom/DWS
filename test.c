1d711bfe043979bfd3a2ac3a38e42fe9bb489e29
Pridom <pridom1118@gmail.com>
2019-11-13 18:33:41 +0900

    change in signal handler

--- test.c
+++ test.c
@@ -4,27 +4,26 @@
 #include <unistd.h>
 #include <signal.h>
 
-struct tm* newTime;
+struct tm* newTime; 
+struct tm* myTime;
 time_t rawtime, testtime;
 
+//ctrl + c displays a new clock 
 void handler(int sig) {
 	char buffer[80];
 	time(&testtime);
 	newTime = localtime(&testtime);
 
 	newTime->tm_year = 2019;
-	newTime->tm_mon = 9;
+	newTime->tm_mon = 8;
 	newTime->tm_mday = 18;
 	newTime->tm_hour = 13;
 	newTime->tm_min = 24;
 	newTime->tm_sec = 50;
 
+	mktime(newTime);
 
 	while(1){
-//		if(getchar() == 'c') {
-//			exit(0);
-//		}
-
 		system("clear");
 
 		time(&testtime);
@@ -47,27 +46,18 @@ void getTime(struct tm* currentTime){
 
 
 int main(){
-	struct tm* myTime;
 	char buffer[80];
 
 	time(&rawtime);
 	myTime = localtime(&rawtime);
 
 	myTime->tm_year = 2019;
-	myTime->tm_mon = 11;
+	myTime->tm_mon = 10;
 	myTime->tm_mday = 14;
 	myTime->tm_hour = 23;
 	myTime->tm_min = 59;
 	myTime->tm_sec = 50;
 	
-	newTime = localtime(&rawtime);
-	newTime->tm_year = 2019;
-	newTime->tm_mon = 9;
-	newTime->tm_mday = 18;
-	newTime->tm_hour = 13;
-	newTime->tm_min = 24;
-	newTime->tm_sec = 50;
-
 	mktime(myTime);
 
 	signal(SIGINT, handler);
