40283c46359a8f8e0b72469bee51907e4ba60b33
Pridom <pridom1118@gmail.com>
2019-11-15 18:41:51 +0900

    segfault solved

---
+++ inputTest.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+//saving current time info in struct tm;
+
+struct tm* newTime; 
+struct tm* myTime;
+time_t rawtime, testtime;
+
+//ctrl + c displays a new clock 
+/*
+void handler(int sig) {
+	char buffer[80];
+	time(&testtime);
+	newTime = localtime(&testtime);
+
+	newTime->tm_year = 2019;
+	newTime->tm_mon = 8;
+	newTime->tm_mday = 18;
+	newTime->tm_hour = 13;
+	newTime->tm_min = 24;
+	newTime->tm_sec = 50;
+
+	mktime(newTime);
+
+	while(1){
+		system("clear");
+
+		time(&testtime);
+	    newTime->tm_sec++;
+
+	    mktime(newTime);
+		strftime(buffer, 80, "%a %m-%d \n %H:%M:%S \a", newTime);
+		puts(buffer);
+		sleep(1);
+	}
+}
+*/
+
+int main(){
+	char buffer[80];
+	int hour, min, sec, year, mon, day;
+
+	time(&rawtime);
+	myTime = localtime(&rawtime);
+
+	/*
+	myTime->tm_year = 2019;
+	myTime->tm_mon = 10;
+	myTime->tm_mday = 14;
+	myTime->tm_hour = 23;
+	myTime->tm_min = 59;
+	myTime->tm_sec = 50;
+	*/
+	printf("Hour - Min - Sec: ");
+	scanf("%d %d %d", &hour, &min, &sec);
+	fflush(stdin);
+	printf("YYYY-MM-DD: ");
+	scanf("%d %d %d", &year, &mon, &day);
+	fflush(stdin);
+
+	myTime->tm_year = year - 1900;
+	myTime->tm_mon = mon - 1;
+	myTime->tm_mday = day;
+	myTime->tm_hour = hour;
+	myTime->tm_min = min;
+	myTime->tm_sec = sec;
+
+	mktime(myTime);
+
+//	signal(SIGINT, handler);
+
+	while(1){
+		system("clear");
+		time(&rawtime);
+	    myTime->tm_sec++;
+
+	    mktime(myTime);
+		strftime(buffer, 80, "%a %y-%m-%d \n %H:%M:%S \a", myTime);
+		puts(buffer);
+		sleep(1);
+	}
+	return 0;
+}
